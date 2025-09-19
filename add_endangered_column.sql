3ed339a699f5a313f6fc000ab882251ea5e3431b
Juliana Li <julianali@dhcp-10-250-37-73.harvard.edu>
2025-09-18 23:38:40 -0400

    Clean up: Remove duplicate and unnecessary files
    - Deleted add_comments_table.sql (duplicate)
    - Deleted add_endangered_column.sql (duplicate)
    - Deleted sample_animals.csv.csv (duplicate file)

--- add_endangered_column.sql
+++
@@ -1,5 +0,0 @@
--- Add endangered column to species table
-ALTER TABLE species ADD COLUMN endangered boolean DEFAULT false;
-
--- Update existing seeded species to have endangered = false
-UPDATE species SET endangered = false;
